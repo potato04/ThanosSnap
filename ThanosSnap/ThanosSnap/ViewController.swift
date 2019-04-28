f9deb9e7e933b4bd1c456f7b1ab795cb8357bd92
potato04 <shiww@outlook.com>
2019-04-29 00:39:02 +0800

    initial project

---
+++ ThanosSnap/ThanosSnap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThanosSnap
+//
+//  Created by potato04 on 2019/4/29.
+//  Copyright © 2019 potato04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
